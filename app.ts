3349afdbb6b6ec3e606330518e44883b36b69a91
SJcodeML <sidrajabin56@gmail.com>
2024-09-08 09:12:14 +0500

    added some

--- app.ts
+++ app.ts
@@ -1,30 +1,3 @@
 
 
 
-function addNewWEField() {  
-  let newNode = document.createElement("textarea");  
-  newNode.classList.add("weField");  
-  newNode.classList.add("multiplelabel");  
-  newNode.classList.add("nameField");  
-  newNode.setAttribute("placeholder", "Enter Here"); // Set placeholder for the new textarea  
-  
-  let weOb = document.getElementById("we");  
-  let weAddButtonOb:HTMLElement|null = document.getElementById("weAddButton");  
-
-  // Insert the new textarea before the button's parent div  
-  weOb?.insertBefore(newNode, weAddButtonOb);  
-}
-
-function addNewAQField() {  
-  let newNode = document.createElement("textarea");  
-  newNode.classList.add("eqField");  
-  newNode.classList.add("multiplelabel");  
-  newNode.classList.add("nameField");  
-  newNode.setAttribute("placeholder", "Enter Here"); // Set placeholder for the new textarea  
-  
-  let aqOb = document.getElementById("orr");  
-  let aqAddButtonOb:HTMLElement|null = document.getElementById("aqAddButton");  
-
-  // Insert the new textarea before the button's parent div  
-  aqOb?.insertBefore(newNode, aqAddButtonOb);  
-}
